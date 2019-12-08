63db9f460a34d85f8f72755a0324e0ae6dcd6152
niktariy <vervaspro@icloud.com>
2019-12-08 13:23:50 +0300

    change default theme

--- src/index.js
+++ src/index.js
@@ -3,26 +3,44 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import * as serviceWorker from './serviceWorker';
 
 import rootReducer from './_reducers';
 
 import App from './App';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#7e57c2',
+    },
+    secondary: {
+      light: '#f6a5c0',
+      main: '#f48fb1',
+      dark: '#f381a7',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
