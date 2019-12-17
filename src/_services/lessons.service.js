108bb1eb4e98c9f3da0ef3221959b968dc30b71c
niktariy <vervaspro@icloud.com>
2019-12-17 22:52:40 +0300

    rename constant for lesson and user api url prefix

--- src/_services/lessons.service.js
+++ src/_services/lessons.service.js
@@ -1,16 +1,19 @@
 import Api from '../api/config';
 
-const API_URL = 'lesson';
+const API_PREFIX = 'lesson';
 
 const getLessonsCourse = courseId => Api().get(`course/${courseId}/lessons`);
 
-const getLessonInfo = lessonId => Api().get(`${API_URL}/${lessonId}`);
+const getLessonInfo = lessonId => Api().get(`${API_PREFIX}/${lessonId}`);
 
 const createLesson = (courseId, lessonData) =>
   Api().post(`course/${courseId}/create_lesson`, lessonData);
+
 const updateLesson = (lessonId, lessonData) =>
-  Api().put(`${API_URL}/${lessonId}/update`, lessonData);
-const deleteLesson = lessonId => Api().delete(`${API_URL}/${lessonId}/delete`);
+  Api().put(`${API_PREFIX}/${lessonId}/update`, lessonData);
+
+const deleteLesson = lessonId =>
+  Api().delete(`${API_PREFIX}/${lessonId}/delete`);
 
 export const lessonService = {
   getLessonsCourse,
--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -1,19 +1,20 @@
 import Api from '../api/config';
 
-const API_URL = '/user_profile';
+const API_PREFIX = 'user_profile';
 
-const getAllUsers = () => Api().get(`${API_URL}/all`);
+const getAllUsers = () => Api().get(`${API_PREFIX}/all`);
 
-const getLoggedUser = token => Api().get(`${API_URL}`);
+const getLoggedUser = token => Api().get(`${API_PREFIX}`);
 
-const getUserProfile = userId => Api().get(`${API_URL}/${userId}`);
+const getUserProfile = userId => Api().get(`${API_PREFIX}/${userId}`);
 
-const getUserRole = userId => Api().get(`${API_URL}/${userId}/role`);
+const getUserRole = userId => Api().get(`${API_PREFIX}/${userId}/role`);
 
-const updateLoggedUser = userData => Api().put(`${API_URL}/update`, userData);
+const updateLoggedUser = userData =>
+  Api().put(`${API_PREFIX}/update`, userData);
 
 const updateUserPassword = newPswd =>
-  Api().put(`${API_URL}/edit_password`, newPswd);
+  Api().put(`${API_PREFIX}/edit_password`, newPswd);
 
 export const userService = {
   getAllUsers,
