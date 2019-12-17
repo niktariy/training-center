2ef9350513c8145008fa558cc66a75f222bacc34
niktariy <vervaspro@icloud.com>
2019-12-17 22:48:56 +0300

    rename constant for course api url prefix

--- src/_services/courses.service.js
+++ src/_services/courses.service.js
@@ -1,17 +1,20 @@
 import Api from '../api/config';
 
-const API_URL = 'course';
+const API_PREFIX = 'course';
 
-const getAllCourses = () => Api().get(`${API_URL}/all`);
+const getAllCourses = () => Api().get(`${API_PREFIX}/all`);
 
-const getCourseById = courseId => Api().get(`${API_URL}/${courseId}`);
+const getCourseById = courseId => Api().get(`${API_PREFIX}/${courseId}`);
 
-const enrollCourse = courseId => Api().post(`${API_URL}/${courseId}/subscribe`);
-const leaveCourse = courseId => Api().post(`${API_URL}/${courseId}/unsubscribe`);
+const enrollCourse = courseId =>
+  Api().post(`${API_PREFIX}/${courseId}/subscribe`);
+const leaveCourse = courseId =>
+  Api().post(`${API_PREFIX}/${courseId}/unsubscribe`);
 
-const createCourse = courseData => Api().post(`${API_URL}/create`, courseData);
+const createCourse = courseData =>
+  Api().post(`${API_PREFIX}/create`, courseData);
 const updateCourse = (courseId, courseData) =>
-  Api().put(`${API_URL}/${courseId}/update`, courseData);
+  Api().put(`${API_PREFIX}/${courseId}/update`, courseData);
 
 const getLecturerCourses = () => Api().get(`lecturer/my_courses`);
 const getListenerCourses = () => Api().get(`listener/my_courses`);
