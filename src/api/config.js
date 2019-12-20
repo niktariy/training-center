30e67002af4a6f8ba1e5f6e5046306e9ec9048af
niktariy <vervaspro@icloud.com>
2019-12-20 17:02:29 +0300

    refactoring

--- src/api/config.js
+++ src/api/config.js
@@ -5,7 +5,7 @@ const cors_api_url = 'https://' + cors_api_host + '/';
 const ngrok_server_number = 'c6b6d866';
 
 export const ENDPOINT =
-  cors_api_url + 'http://' + ngrok_server_number + '.ngrok.io';
+  cors_api_url + 'http://training-center-boot.herokuapp.com';
 
 const api = () => axios.create({
   baseURL: ENDPOINT,
